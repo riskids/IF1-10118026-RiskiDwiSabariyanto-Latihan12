60f5d20f56db28789a5804faa6357322ded3aacf
Riski Dwi Sabariyanto <bintangmalamhari@gmail.com>
2020-01-07 21:58:07 +0700

    Add files via upload

---
+++ IF1-10118026-RiskiDwiSabariyanto-Latihan12/src/edu/riskidwi/latihanmvc/model/IF110118026riskidwilatihan12.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.riskidwi.latihanmvc.model;
+
+import edu.riskidwi.latihanmvc.view.PlangganView;
+
+/**
+ *
+ * @author Guntur Maulana I
+ */
+public class IF110118026riskidwilatihan12 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        PlangganView view = new PlangganView();
+        view.setVisible(true);
+        
+    }
+    
+}
